f8b4f07fb3b1c82824c12264e0216beb50fa00f7
VinaySingla896 <vinay.s@wizcommmerce.com>
2025-03-21 22:37:42 +0530

    Updated experience

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -23,7 +23,7 @@ export function Hero() {
           <h3>Software Developer</h3>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">5 months of Experience</p>
+          <p className="small-resume">6 months of Experience</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
           <BrowserRouter>
