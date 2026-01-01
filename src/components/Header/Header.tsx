84b9d5f06587dd4c44a2fb578a071d94e19112db
Vinay Singla <vinaysingla@Vinays-MacBook-Air.local>
2026-01-01 16:16:12 +0530

    Added experience to navbar also

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -33,6 +33,9 @@ export function Header() {
           <NavHashLink smooth to="#about" onClick={closeMenu}>
             About me
           </NavHashLink>
+          <NavHashLink smooth to="#experience" onClick={closeMenu}>
+            Experience
+          </NavHashLink>
           <NavHashLink smooth to="#project" onClick={closeMenu}>
             Project
           </NavHashLink>
