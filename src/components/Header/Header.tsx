5d0f6d2719f90f01b7f1a18d6fc731355ed3616a
Vinay Singla <vinaysingla1235@gmail.com>
2026-09-12 21:41:27 +0530

    Updated resume

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -45,7 +45,7 @@ export function Header() {
           <NavHashLink smooth to="#contact" onClick={closeMenu}>
             Contact
           </NavHashLink>
-          <a href="https://drive.google.com/file/d/1JvMPCjCDqsGwWjMyAhZQ8OwHF__7Z5zV/view?usp=sharing" target="_blank" download className="button">
+          <a href="https://drive.google.com/file/d/1JsTz8BQOoO92wqYEfjoXm1lLuF1T9vqB/view?usp=sharing" target="_blank" download className="button">
             Resume
           </a>
         </nav>
